de156aec7d8d57d6258d7f424eb54f47d6e249b8
HRITHIKRAJ <hrithikrajprasad246@gmail.com>
2021-06-05 23:35:13 +0530

    commit

---
+++ src/main/java/com/xfactor/lably/controllers/CustomerController.java
@@ -0,0 +1,62 @@
+package com.xfactor.lably.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.xfactor.lably.entity.Customer;
+import com.xfactor.lably.repository.CustomerRepository;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    ArrayList<Customer> customer = new ArrayList<>();
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping("/getCustomer")
+    public ArrayList<Customer> getCustomer() {
+        return customer;
+    }
+
+    @PostMapping("/addCustomer")
+    public Customer addCustomer(@RequestBody Customer customer) {
+
+        Customer persistedCustomer = customerRepository.save(customer);
+        return persistedCustomer;
+    }
+
+    @GetMapping("/getAllCustomer")
+    public List<Customer> getCustomers(){
+        List<Customer> persistedCustomer = customerRepository.findAll();
+        return persistedCustomer;
+    }
+
+    @GetMapping("/search")
+    public ArrayList<Customer> searchAdmin(@RequestParam String name){
+
+        ArrayList<Customer> ad= new ArrayList<>();
+        boolean f = false;
+
+        for(Customer a: customer){
+            if(a.getName().equals(name)){
+                f = true;
+                ad.add(a);
+            }              
+        }
+        if(f==true) return ad;
+        else return null;
+    }
+
+}
